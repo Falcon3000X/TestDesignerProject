8c9e5d73120c2d63ff65d97c74eee3ed62b5e436
Mark Brighton <marik.shevchenko@icloud.com>
2020-11-24 20:31:37 +0200

    Upgrade 1.9.1 (Add groupBox for User)

--- ServerSideProject/Form1.Designer.cs
+++ ServerSideProject/Form1.Designer.cs
@@ -39,8 +39,21 @@ namespace ServerSideProject
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.buttonUserToGroup = new System.Windows.Forms.Button();
-            this.labelCourse = new System.Windows.Forms.Label();
+            this.labelUser = new System.Windows.Forms.Label();
+            this.labelTest = new System.Windows.Forms.Label();
+            this.buttonTestToGroup = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBoxTests = new System.Windows.Forms.ListBox();
+            this.groupBoxUser = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxUserLogin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBoxMenu.SuspendLayout();
+            this.groupBoxUser.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonAddUser
@@ -63,12 +76,13 @@ namespace ServerSideProject
             this.buttonAddGroup.TabIndex = 1;
             this.buttonAddGroup.Text = "Create group";
             this.buttonAddGroup.UseVisualStyleBackColor = true;
+            this.buttonAddGroup.Click += new System.EventHandler(this.buttonAddGroup_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MingLiU-ExtB", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(387, 285);
+            this.label1.Location = new System.Drawing.Point(818, 503);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(295, 48);
             this.label1.TabIndex = 2;
@@ -97,11 +111,12 @@ namespace ServerSideProject
             this.button1.TabIndex = 2;
             this.button1.Text = "Remove user";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBoxUsers
             // 
             this.listBoxUsers.FormattingEnabled = true;
-            this.listBoxUsers.Location = new System.Drawing.Point(562, 12);
+            this.listBoxUsers.Location = new System.Drawing.Point(994, 12);
             this.listBoxUsers.Name = "listBoxUsers";
             this.listBoxUsers.Size = new System.Drawing.Size(120, 186);
             this.listBoxUsers.TabIndex = 4;
@@ -109,7 +124,7 @@ namespace ServerSideProject
             // listBoxGroups
             // 
             this.listBoxGroups.FormattingEnabled = true;
-            this.listBoxGroups.Location = new System.Drawing.Point(315, 52);
+            this.listBoxGroups.Location = new System.Drawing.Point(747, 52);
             this.listBoxGroups.Name = "listBoxGroups";
             this.listBoxGroups.Size = new System.Drawing.Size(120, 186);
             this.listBoxGroups.TabIndex = 5;
@@ -118,7 +133,7 @@ namespace ServerSideProject
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(318, 12);
+            this.label2.Location = new System.Drawing.Point(750, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(117, 32);
             this.label2.TabIndex = 6;
@@ -128,7 +143,7 @@ namespace ServerSideProject
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(572, 201);
+            this.label3.Location = new System.Drawing.Point(1004, 201);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(100, 32);
             this.label3.TabIndex = 7;
@@ -137,30 +152,151 @@ namespace ServerSideProject
             // buttonUserToGroup
             // 
             this.buttonUserToGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonUserToGroup.Location = new System.Drawing.Point(458, 95);
+            this.buttonUserToGroup.Location = new System.Drawing.Point(890, 95);
             this.buttonUserToGroup.Name = "buttonUserToGroup";
             this.buttonUserToGroup.Size = new System.Drawing.Size(81, 23);
             this.buttonUserToGroup.TabIndex = 3;
             this.buttonUserToGroup.Text = "User to group";
             this.buttonUserToGroup.UseVisualStyleBackColor = true;
             // 
-            // labelCourse
+            // labelUser
             // 
-            this.labelCourse.AutoSize = true;
-            this.labelCourse.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCourse.Location = new System.Drawing.Point(456, 121);
-            this.labelCourse.Name = "labelCourse";
-            this.labelCourse.Size = new System.Drawing.Size(83, 32);
-            this.labelCourse.TabIndex = 8;
-            this.labelCourse.Text = "<---";
+            this.labelUser.AutoSize = true;
+            this.labelUser.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUser.Location = new System.Drawing.Point(888, 121);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(83, 32);
+            this.labelUser.TabIndex = 8;
+            this.labelUser.Text = "<---";
+            // 
+            // labelTest
+            // 
+            this.labelTest.AutoSize = true;
+            this.labelTest.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTest.Location = new System.Drawing.Point(626, 164);
+            this.labelTest.Name = "labelTest";
+            this.labelTest.Size = new System.Drawing.Size(83, 32);
+            this.labelTest.TabIndex = 14;
+            this.labelTest.Text = "--->";
+            // 
+            // buttonTestToGroup
+            // 
+            this.buttonTestToGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTestToGroup.Location = new System.Drawing.Point(628, 138);
+            this.buttonTestToGroup.Name = "buttonTestToGroup";
+            this.buttonTestToGroup.Size = new System.Drawing.Size(81, 23);
+            this.buttonTestToGroup.TabIndex = 9;
+            this.buttonTestToGroup.Text = "Test to group";
+            this.buttonTestToGroup.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MingLiU-ExtB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(488, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 32);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tests";
+            // 
+            // listBoxTests
+            // 
+            this.listBoxTests.FormattingEnabled = true;
+            this.listBoxTests.Location = new System.Drawing.Point(485, 95);
+            this.listBoxTests.Name = "listBoxTests";
+            this.listBoxTests.Size = new System.Drawing.Size(120, 186);
+            this.listBoxTests.TabIndex = 11;
+            // 
+            // groupBoxUser
+            // 
+            this.groupBoxUser.Controls.Add(this.checkBox1);
+            this.groupBoxUser.Controls.Add(this.label5);
+            this.groupBoxUser.Controls.Add(this.textBox1);
+            this.groupBoxUser.Controls.Add(this.label4);
+            this.groupBoxUser.Controls.Add(this.textBoxUserLogin);
+            this.groupBoxUser.Controls.Add(this.button2);
+            this.groupBoxUser.Enabled = false;
+            this.groupBoxUser.Location = new System.Drawing.Point(94, 202);
+            this.groupBoxUser.Name = "groupBoxUser";
+            this.groupBoxUser.Size = new System.Drawing.Size(210, 207);
+            this.groupBoxUser.TabIndex = 15;
+            this.groupBoxUser.TabStop = false;
+            this.groupBoxUser.Text = "User";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(115, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Confirm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxUserLogin
+            // 
+            this.textBoxUserLogin.Location = new System.Drawing.Point(59, 44);
+            this.textBoxUserLogin.Name = "textBoxUserLogin";
+            this.textBoxUserLogin.Size = new System.Drawing.Size(141, 20);
+            this.textBoxUserLogin.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Login:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(145, 120);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Admin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(494, 346);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(694, 342);
-            this.Controls.Add(this.labelCourse);
+            this.ClientSize = new System.Drawing.Size(1125, 560);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBoxUser);
+            this.Controls.Add(this.labelTest);
+            this.Controls.Add(this.buttonTestToGroup);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBoxTests);
+            this.Controls.Add(this.labelUser);
             this.Controls.Add(this.buttonUserToGroup);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -171,6 +307,8 @@ namespace ServerSideProject
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBoxMenu.ResumeLayout(false);
+            this.groupBoxUser.ResumeLayout(false);
+            this.groupBoxUser.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,7 +326,19 @@ namespace ServerSideProject
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button buttonUserToGroup;
-        private System.Windows.Forms.Label labelCourse;
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label labelTest;
+        private System.Windows.Forms.Button buttonTestToGroup;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBoxTests;
+        private System.Windows.Forms.GroupBox groupBoxUser;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxUserLogin;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- ServerSideProject/Form1.cs
+++ ServerSideProject/Form1.cs
@@ -13,13 +13,25 @@ namespace ServerSideProject
 {
     public partial class Form1 : Form
     {
-        
+        List<Group> groups = new List<Group>();
+        List<User> users = new List<User>();
+
         public Form1()
         {
             InitializeComponent();
         }
 
         private void buttonAddUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAddGroup_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
